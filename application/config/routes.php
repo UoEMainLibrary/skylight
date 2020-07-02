c7c695255c10496fc14899893bba921b519560e6
WebSepp <bastilange@t-online.de>
2020-07-02 15:16:22 +0100

    added fixes to the advanced search

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,6 +43,9 @@ $route['404_override'] = 'content';
 
 $route['art/(:any)'] = "$1";
 $route['mimed/(:any)'] = "$1";
+$route['geddes/(:any)'] = "$1";
+$route['archivemedia/(:any)'] = "$1";
+$route['speccoll/(:any)'] = "$1";
 $route['physics/(:any)'] = "$1";
 $route['calendars/(:any)'] = "$1";
 $route['cockburn/(:any)'] = "$1";
@@ -57,13 +60,17 @@ $route['towardsdolly/(:any)'] = "$1";
 $route['coimbra/(:any)'] = "$1";
 $route['lhsacasenotes/(:any)'] = "$1";
 $route['guardbook/(:any)'] = "$1";
-$route['archivemedia/(:any)'] = "$1";
-$route['speccoll/(:any)'] = "$1";
-$route['geddes/(:any)'] = "$1";
 $route['public-art/(:any)'] = "$1";
-$route['jlss/(:any)'] = "$1";
+$route['coimbra-colls/(:any)'] = "$1";
+$route['collections/(:any)'] = "$1";
+$route['iconicsdeepzoom/(:any)'] = "$1";
+$route['colllevel/(:any)'] = "$1";
+$route['arch-dig/(:any)'] = "$1";
+$route['umis/(:any)'] = "$1";
+$route['libguides/(:any)'] = "$1";
 $route['bodylanguage/(:any)'] = "$1";
 $route['eerc/(:any)'] = "$1";
+$route['bodylanguage/(:any)'] = "$1";
 $route['sitemap\.xml'] = "sitemap/sitemap";
 
 
